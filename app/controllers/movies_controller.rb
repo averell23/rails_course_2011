c2db3dc59a9c376acbd211ae65d26bf0856b34d0
Daniel Hahn <ghub@limitedcreativity.org>
2011-03-14 16:08:18 +0100

    Adding movies from the web

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,19 @@
 class MoviesController < ApplicationController
   
+  # GET /movies/
   def index
     @movies = Movie.all
+  end
+  
+  # GET /movies/new/
+  def new
+    @movie = Movie.new
+  end
+  
+  def create
+    @movie = Movie.new(params[:movie])
+    @movie.save!
+    redirect_to movies_path
   end
   
 end
